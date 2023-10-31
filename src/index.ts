33c2a555a64a9e11b70f79a8c574194d407f0531
Christopher Wood <chriswood@cloudflare.com>
2023-10-31 12:02:47 +0100

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,151 @@
+import {
+	IssuerConfig,
+	PrivateToken,
+	TOKEN_TYPES,
+	Token,
+	TokenChallenge,
+	util,
+} from '@cloudflare/privacypass-ts';
+import { base64UrlToUint8Array, verifyToken } from './redemption.js';
+
+import originHTML from './origin-html.js';
+import tokenOKHTML from './origin-html-to-remove-token-ok.js';
+import { Bindings } from './bindings.js';
+
+export default {
+	async fetch(request: Request, env: Bindings) {
+		return handleRequest(request, env);
+	},
+};
+
+function generateFetchIssuerEndpoint(
+	env: Bindings
+): (input: RequestInfo, info?: RequestInit) => Promise<Response> {
+	return (input: RequestInfo, info?: RequestInit) => {
+		if (env.ISSUER) {
+			return env.ISSUER.fetch(input, info);
+		}
+		return fetch(input, info);
+	};
+}
+
+/**
+ * Fetch public keys and identifiers from the designated Privacy Pass issuer service.
+ * @param {String} issuerName
+ * @returns a list with three ArrayBuffer elements
+ */
+async function fetchBasicIssuerKeys(env: Bindings, issuerName: string) {
+	const issuerURL = `https://${issuerName}`;
+
+	// Fetch the issuer configuration
+	const init = {
+		headers: {
+			'content-type': 'application/json',
+		},
+	};
+	const configURL = `${issuerURL}/.well-known/token-issuer-directory`;
+	const configResponse = await generateFetchIssuerEndpoint(env)(configURL, init);
+	const config: IssuerConfig = await configResponse.json();
+
+	// Parse out the token keys (in legacy format too)
+	const token = config['token-keys'].find(
+		token => token['token-type'] == TOKEN_TYPES.BLIND_RSA.value
+	);
+
+	if (!token) {
+		throw new Error('Could not find BlindRSA token key on issuer');
+	}
+
+	const clientRequestKeyEnc = base64UrlToUint8Array(token['token-key']);
+
+	return clientRequestKeyEnc;
+}
+
+async function issuerKeys(env: Bindings): Promise<[CryptoKey, Uint8Array]> {
+	// Fetch issuer keys
+	const clientRequestKeyEnc = await fetchBasicIssuerKeys(env, env.ISSUER_NAME);
+	const spkiEnc = util.convertRSASSAPSSToEnc(clientRequestKeyEnc);
+	// Import the public key that we'll use for verification
+	const tokenKey = await crypto.subtle.importKey(
+		'spki',
+		spkiEnc,
+		{
+			name: 'RSA-PSS',
+			hash: { name: 'SHA-384' },
+		},
+		false,
+		['verify']
+	);
+
+	return [tokenKey, clientRequestKeyEnc];
+}
+
+async function handleLogin(request: Request, env: Bindings) {
+	const tokenType = TOKEN_TYPES.BLIND_RSA;
+	let tokenKey, clientRequestKeyEnc;
+	try {
+		[tokenKey, clientRequestKeyEnc] = await issuerKeys(env);
+	} catch (err) {
+		return new Response('Failure to decode token verification key. ' + err, { status: 404 });
+	}
+
+	const fixedRedemptionContext = new Uint8Array(32);
+	fixedRedemptionContext.fill(0xfe);
+	const challenge = new TokenChallenge(tokenType.value, env.ISSUER_NAME, fixedRedemptionContext, [
+		env.ORIGIN_NAME,
+	]);
+	const privateToken = new PrivateToken(challenge, clientRequestKeyEnc, 10);
+
+	// If the request is for the /login resource, check to see if the request
+	// has the WWW-Authenticate header carrying a token.
+	const authenticator = request.headers.get('Authorization') ?? '';
+	if (authenticator.startsWith('PrivateToken token=')) {
+		const tokenChallenge = challenge.serialize();
+		const context = new Uint8Array(await crypto.subtle.digest('SHA-256', tokenChallenge));
+		const token = Token.parse(tokenType, authenticator)[0];
+		token.verify(tokenKey);
+		const valid = await verifyToken(authenticator, tokenKey, context);
+		if (valid) {
+			return new Response(tokenOKHTML(env), {
+				headers: {
+					'content-type': 'text/html;charset=UTF-8',
+				},
+				status: 200,
+			});
+		}
+		return new Response('Token verification failed', {
+			headers: {
+				'content-type': 'text/html;charset=UTF-8',
+			},
+			status: 400,
+		});
+	}
+
+	return new Response(originHTML(env), {
+		headers: {
+			'content-type': 'text/html;charset=UTF-8',
+			'WWW-Authenticate': privateToken.toString(),
+		},
+		status: 401,
+	});
+}
+
+/**
+ * Handle a request to the demo Privacy Pass redemption server.
+ * @param {Request} request
+ */
+async function handleRequest(request: Request, env: Bindings) {
+	// If the request is for the home page, return the basic interaction form.
+	const url = new URL(request.url);
+
+	if (url.pathname.startsWith('/login')) {
+		return handleLogin(request, env);
+	}
+
+	return new Response('Unsupported resource', {
+		headers: {
+			'content-type': 'text/html;charset=UTF-8',
+		},
+		status: 400,
+	});
+}
---
+++ src/origin-html.ts
@@ -0,0 +1,180 @@
+import { Bindings } from './bindings';
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export default (_env: Bindings) => `<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+
+<title>Cloudflare Research | Privacy Pass</title>
+
+<link href="https://research.cloudflare.com/css/standalone1.css" rel="stylesheet" type="text/css">
+
+<script type="module">
+  window.extension_supported = false
+
+  // try to retrieve PrivacyPassExtensionV from Server-Timing header
+  if ( window.performance && performance.getEntriesByType ) {
+    let navTiming = performance.getEntriesByType( 'navigation' )
+    if ( navTiming.length > 0 ) {
+      let serverTiming = navTiming[ 0 ].serverTiming
+      if ( serverTiming && serverTiming.length > 0 ) {
+        for ( let i=0; i < serverTiming.length; i++ ) {
+          if ( serverTiming[ i ].name == 'PrivacyPassExtensionV' && serverTiming[ i ].description.indexOf( '4' ) == 0 )
+            window.extension_supported = true
+        }
+      }
+    }
+  }
+   
+  if ( window.extension_supported ) {
+    document.getElementById( 'a1' ).classList.remove( 'hidden' )
+    document.getElementById( 'snt' ).classList.remove( 'hidden' )
+    document.getElementById( 'b2' ).firstElementChild.classList.add( 'green' )
+    document.getElementById( 'b3' ).firstElementChild.classList.add( 'green' )
+  }
+  else
+    document.getElementById( 'e1' ).classList.remove( 'hidden' )
+
+  console.log( 'window.extension_supported: ' + window.extension_supported )
+
+</script>
+
+<style type="text/css">
+  .header { border-bottom: 0; z-index: 100 }
+  main { padding-right: 2rem; padding-bottom: 2rem }
+  .q {
+  	text-decoration: none;  	
+    border: 1px solid var(--indigo-8);
+    background-color: var(--indigo-9);
+    padding: 0 0.2rem 0 0.2rem;
+    color: black;
+  }
+  .q:hover {
+    cursor: help;
+  	text-decoration: none;
+  	border: 1px solid var(--green-8);
+    background-color: var(--green-9);
+  }
+  .q:visited {
+    color: black;
+  }
+  .current {
+  	display: table;
+  	background-color: var(--orange-8);
+  	border: 1px solid var(--orange-7);
+  	margin-bottom: 0.2rem;
+  }
+  .current .num {
+    margin-left: -1rem; 
+    padding: 1rem 0.8rem 1rem 0.8rem; 
+    background-color:var(--orange-7); 
+    margin-right: 0.8rem;
+  }
+  div.action {
+    position: relative;
+    border-radius: 1rem;
+    background-color: var(--violet-9);
+  	border: 1px solid var(--violet-7);
+  	padding: 0.5rem 1rem 0.5rem 1.5rem;
+  	margin-bottom: 0.2rem;
+    margin-top: -1rem;
+    margin-left: 1.7rem;
+  }
+  div.error {
+    position: relative;
+    border-radius: 1rem;
+    color: white;
+    background-color: var(--indigo-2);
+  	border: 1px solid var(--indigo-1);
+  	padding: 0.5rem 1rem 0.5rem 1.5rem;
+  	margin-bottom: 0.2rem;
+    margin-top: -1rem;
+    margin-left: 1.7rem;  	
+  }
+  div.error a {
+  	color: white;
+  }
+  .box {
+  	display: table;
+  	background-color: white;
+  	border: 1px solid var(--orange-8);
+  	margin-bottom: 0.2rem;
+  }
+  .box .num {
+    margin-left: -1rem; 
+    padding: 1rem 0.8rem 1rem 0.8rem; 
+    background-color:var(--orange-8); 
+    margin-right: 0.8rem;
+  }
+  .snt {
+    margin-top: 1rem;
+    margin-right: -1rem;
+  	float: right; 
+  	background-color: green; 
+  	color: white; 
+  	width: 1.9rem;
+  	height: 3rem; 
+  	display: flex; 
+  	justify-content: center; 
+  	flex-direction: column; 
+  	border-top: 1px solid var(--orange-8);
+  	border-bottom: 1px solid var(--orange-8);
+  }
+  .hidden {
+  	display: none;
+  }
+  .num.green {
+  	background-color: green;
+  	color: white;
+  }  
+</style>
+
+</head>
+
+<body>
+
+<div class="wrapper" style="position: relative">
+<header class="header">
+  <a href="https://research.cloudflare.com"><img class="logo" src="https://research.cloudflare.com/img/logo.svg" alt="Cloudflare Research logo"></a>
+  <a href="/" style="text-decoration: none"><span style="font-size: 1.4rem">Privacy Pass</span></a><!-- &nbsp;<small>"Privacy Pass with Private Access Tokens"</small> -->
+  <span class="flare" style="background-color: var(--violet-2); color: white;">DEMO</span>
+  <div style="position: absolute; display: block; border: 0; padding: 0; margin: 0; width: 100%; height: 0.2rem; top: 3.6rem; background: linear-gradient(70deg, var(--green-8) 30%, rgba(0,0,0,0) 30%), linear-gradient(30deg, var(--indigo-8) 60%, var(--orange-8) 60%);">&nbsp;</div>
+</header>
+
+<div class="description">
+  <br>
+  <div class="current" id="b1"><span class="num">1</span>Origin returns WWW-Authenticate header</div>
+  <div class="action hidden" id="a1">Browser extension opens tab to Attester</div>
+  <div class="error hidden" id="e1">Missing <a href="https://chrome.google.com/webstore/detail/privacy-pass/ajhmfdgkijocedmfjonnpjfojldioehi">browser extension</a></div>
+  <div class="snt hidden" id="snt">See new tab</div>
+  <div class="box" id="b2"><span class="num">2</span>Attester presents challenge</div>
+  <div class="box" id="b3"><span class="num">3</span>Issuer issues token</div>
+  <div class="box" id="b4"><span class="num">4</span>New request to Origin made with token</div>
+  <br><br>
+</div>
+
+<main>
+  <br>
+  <h2><a class="q" id="h" target="_blank" href="https://blog.cloudflare.com/eliminating-captchas-on-iphones-and-macs-using-new-standard/">Privacy Pass Token</a> &#128721; not presented</h2>
+  <br>
+  This origin response contains a <a class="q" target="_blank" href="https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/WWW-Authenticate">WWW-Authenticate</a> HTTP header with a
+  <a class="q" target="_blank" href="https://www.ietf.org/archive/id/draft-ietf-privacypass-auth-scheme-01.html">PrivateToken</a> challenge. <br><br>
+  Browsers that support this authentication method may respond with a <a class="q" target="_blank" href="https://blog.cloudflare.com/eliminating-captchas-on-iphones-and-macs-using-new-standard/">Privacy Pass Token</a> to authenticate. <br><br>  
+  The <a href="https://chrome.google.com/webstore/detail/privacy-pass/ajhmfdgkijocedmfjonnpjfojldioehi">Privacy Pass extension</a> adds Privacy Pass support to your browser that opens an <a class="q" target="_blank" href="https://www.ietf.org/archive/id/draft-ietf-privacypass-architecture-09.html#name-attester-role">Attester</a> to issue a challenge.
+</main>
+
+
+<footer class="footer" style="z-index: 2; background-color: white">
+  <a class="plain" href="https://www.cloudflare.com"> Copyright &copy; <span id="year">2021</span>&nbsp;Cloudflare, Inc.&nbsp;</a>
+  <script>document.getElementById( 'year' ).innerHTML = ( new Date().getFullYear() )</script>
+  <a href="https://research.cloudflare.com/contact/">Contact us</a>
+  <a href="https://www.cloudflare.com/privacypolicy/">Privacy policy</a>
+  <a href="https://www.cloudflare.com/website-terms/">Terms of service</a>
+</footer>
+</div>
+
+</body>
+
+</html>
+`;
