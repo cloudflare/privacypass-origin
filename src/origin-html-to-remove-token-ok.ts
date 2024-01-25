407c07c3d2577c565f9546c5bc502178f528ece1
Thibault Meunier <thibault@cloudflare.com>
2024-01-25 11:15:28 +0100

    Add favicon to origin web page

--- src/origin-html-to-remove-token-ok.ts
+++ src/origin-html-to-remove-token-ok.ts
@@ -7,6 +7,7 @@ export default (_env: Bindings) => `<!DOCTYPE html>
 <meta charset="utf-8">
 
 <title>Cloudflare Research | Privacy Pass</title>
+<link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🎟️</text></svg>">
 
 <link href="https://research.cloudflare.com/css/standalone1.css" rel="stylesheet" type="text/css">
 
